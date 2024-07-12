bfb8b2102fe63878edd541dca08742ba536370bf
2075 <2075@zero.io>
2024-07-13 03:20:22 +0800

    feat: add dapp proxy contract

---
+++ HelloWorld.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/* 
+dApp staking whitelisted contract
+
+Project overview:
+GameDAO, a next generation video games publisher
+providing fundraising, coordination, engagement and marketplace protocols
+for games, arts, media, engineering.
+Let's build better games together!
+
+We are part of the builders program, check out our proposal here:
+https://forum.astar.network/t/gamedao-unstoppable-grants-dapp-staking-application/7020
+
+You can find us here:
+Telegram: @gamedaoco
+Discord: discord.gg/gamedao
+Twitter: @gamedaoco
+
+*/
+
+contract helloWorld {
+    function renderHelloWorld() public pure returns (string memory) {
+        return "helloWorld";
+    }
+}
